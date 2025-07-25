028dfb5996a1b0fa0e297694b1cce95e74ff6ec4
JEEVARAJ <jeevaraj@JEEVARAJs-MacBook-Air.local>
2025-07-25 12:29:19 +0530

    Updated  discountlabel.spec.js

--- tests/discountlabel.spec.js
+++ tests/discountlabel.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 import path from 'path';
 
-test.only('Discountlabel SiteMonitoring orderFlow', async ({ page }) => { 
+test('Discountlabel SiteMonitoring orderFlow', async ({ page }) => { 
   await page.goto('https://www.discountlabels.com/User/Login/');
   await page.locator("#username").fill("test182@mailinatro.com",{timeout: 10000});
   await page.locator("#password").fill("Test@123",{timeout: 10000});
